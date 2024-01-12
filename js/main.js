ed3db43daea926bdcd455daf69d69fe6ae46aa25
Elmira Loghmani <elmiiiraloq@gmail.com>
2024-01-12 13:41:24 +0330

    Add footer js codes to main.js

--- js/main.js
+++ js/main.js
@@ -7,6 +7,13 @@ Container();
 import Paragraph from "./Paragraph.js";
 Paragraph();
 
+import Footer from "./Footer.js";
+Footer();
+
+import FooterMore from "./FooterMore.js";
+FooterMore();
+
+
 // when we click on the menu icon in small sizes
 let flag = true;
 $("div.wrapper>div.header>div.vertical-menu>div.menu").on({
@@ -137,3 +144,161 @@ $("div.wrapper>div.header>div.vertical-menu>div.menu").on({
     }
   },
 });
+
+
+// About us footer menu
+let flag2 = true;
+$("div.wrapper>div.footer>div.f-menu>nav").on(
+  "click",
+  "li#about-us",
+  function () {
+    console.log("hi");
+
+    if (flag2) {
+      $("div.wrapper>div.footer>div.f-menu>nav>li#about-us>div").slideDown(500);
+
+      $("div.wrapper>div.footer>div.f-menu>nav>li#about-us>i").css({
+        transform: "rotate(180deg)",
+        transition: "250ms",
+      });
+      flag2 = false;
+    } else {
+      $("div.wrapper>div.footer>div.f-menu>nav>li#about-us>div").slideUp(500);
+
+      $("div.wrapper>div.footer>div.f-menu>nav>li#about-us>i").css({
+        transform: "rotate(0deg)",
+        transition: "250ms",
+      });
+      flag2 = true;
+    }
+  }
+);
+
+// Careers footer menu
+let flag3 = true;
+$("div.wrapper>div.footer>div.f-menu>nav").on(
+  "click",
+  "li#careers",
+  function () {
+    console.log("hi");
+
+    if (flag3) {
+      $("div.wrapper>div.footer>div.f-menu>nav>li#careers>div").slideDown(500);
+
+      $("div.wrapper>div.footer>div.f-menu>nav>li#careers>i").css({
+        transform: "rotate(180deg)",
+        transition: "250ms",
+      });
+      flag3 = false;
+    } else {
+      $("div.wrapper>div.footer>div.f-menu>nav>li#careers>div").slideUp(500);
+
+      $("div.wrapper>div.footer>div.f-menu>nav>li#careers>i").css({
+        transform: "rotate(0deg)",
+        transition: "250ms",
+      });
+      flag3 = true;
+    }
+  }
+);
+
+// Social Impact footer menu
+let flag4 = true;
+$("div.wrapper>div.footer>div.f-menu>nav").on(
+  "click",
+  "li#social-impact",
+  function () {
+    console.log("hi");
+
+    if (flag4) {
+      $("div.wrapper>div.footer>div.f-menu>nav>li#social-impact>div").slideDown(
+        500
+      );
+
+      $("div.wrapper>div.footer>div.f-menu>nav>li#social-impact>i").css({
+        transform: "rotate(180deg)",
+        transition: "250ms",
+      });
+
+      flag4 = false;
+    } else {
+      $("div.wrapper>div.footer>div.f-menu>nav>li#social-impact>div").slideUp(
+        500
+      );
+
+      $("div.wrapper>div.footer>div.f-menu>nav>li#social-impact>i").css({
+        transform: "rotate(0deg)",
+        transition: "250ms",
+      });
+      flag4 = true;
+    }
+  }
+);
+
+// For Business Partners footer menu
+let flag5 = true;
+$("div.wrapper>div.footer>div.f-menu>nav").on(
+  "click",
+  "li#business-partners",
+  function () {
+    console.log("hi");
+
+    if (flag5) {
+      $(
+        "div.wrapper>div.footer>div.f-menu>nav>li#business-partners>div"
+      ).slideDown(500);
+
+      $("div.wrapper>div.footer>div.f-menu>nav>li#business-partners>i").css({
+        transform: "rotate(180deg)",
+        transition: "250ms",
+      });
+
+      flag5 = false;
+    } else {
+      $(
+        "div.wrapper>div.footer>div.f-menu>nav>li#business-partners>div"
+      ).slideUp(500);
+
+      $("div.wrapper>div.footer>div.f-menu>nav>li#business-partners>i").css({
+        transform: "rotate(0deg)",
+        transition: "250ms",
+      });
+
+      flag5 = true;
+    }
+  }
+);
+
+// Order and Pick Up footer menu
+let flag6 = true;
+$("div.wrapper>div.footer>div.f-menu>nav").on(
+  "click",
+  "li#order-pickup",
+  function () {
+    console.log("hi");
+
+    if (flag6) {
+      $("div.wrapper>div.footer>div.f-menu>nav>li#order-pickup>div").slideDown(
+        500
+      );
+
+      $("div.wrapper>div.footer>div.f-menu>nav>li#order-pickup>i").css({
+        transform: "rotate(180deg)",
+        transition: "250ms",
+      });
+
+      flag6 = false;
+    } else {
+      $("div.wrapper>div.footer>div.f-menu>nav>li#order-pickup>div").slideUp(
+        500
+      );
+
+      $("div.wrapper>div.footer>div.f-menu>nav>li#order-pickup>i").css({
+        transform: "rotate(0deg)",
+        transition: "250ms",
+      });
+
+      flag6 = true;
+    }
+  }
+);
